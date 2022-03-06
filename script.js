afe8c4c3fbb1fea4c314016bd12c26ebd5e0145f
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-03-05 20:27:39 -0700

    css finalized

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Assignment code here
+const characterAmountRange = document.getElementById
+('characterAmountRange')
+const characterAmountNumber = document.getElementById
+('characterAmountNumber')
+const includeUppercaseElement = document.getElementById
+('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includSymbols')
+const form = document.getElementById('passwordGeneratorForm')
 
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, 
+    includeNumbers, includeSymbols)
+})
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
